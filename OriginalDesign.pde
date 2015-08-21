8d6175f742897b3c4d110b2974458625f73667cb
eryup <eric.yup1@gmail.com>
2015-08-21 11:37:40 -0700

    yeah

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,11 @@
 void setup()
 {
-
+	size(1000,800);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+	background(0,30);
+  ellipse(mouseX,mouseY,50,50);
+  bezier(0, 0, .75*mouseX, .75*mouseY, 1.5*mouseX, 1.5*mouseY, 1000, 800);
 }
 
---
+++ build-tmp/source/OriginalDesign.java
@@ -0,0 +1,36 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
+{
+	size(1000,800);
+}
+public void draw()
+{
+	background(0,30);
+  ellipse(mouseX,mouseY,50,50);
+  bezier(0, 0, .75f*mouseX, .75f*mouseY, 1.5f*mouseX, 1.5f*mouseY, 1000, 800);
+}
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
