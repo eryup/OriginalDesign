b5d56714f1ab1b0a3de132f078b94d4f6ff5931e
unknown <eryup@697-334-Aldburg.sf.k12.internal>
2015-08-27 11:36:42 -0700

    yee

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,11 +1,45 @@
 void setup()
 {
-	size(1000,800);
+	size(startX,startY);
 }
+int startX = 1000;
+int startY = 800;
+int multiplierInt = 2;
+int orth = 300;
+int colorX = 25;
+int colorY = 139;
+int colorZ = 2;
+//boolean swap = false;
+//float square = sq(multiplierInt);
+float multiplier1 = multiplierInt/4;
+//float multiplier1 = multiplierInt/4;
 void draw()
 {
-	background(0,30);
-  ellipse(mouseX,mouseY,50,50);
-  bezier(0, 0, .75*mouseX, .75*mouseY, 1.5*mouseX, 1.5*mouseY, 1000, 800);
+design();
+}
+void mousePressed ()
+{
+	colorSwap ();
+	//swap=true;
 }
 
+void design ()
+{
+	//background(0,30,30);
+	//fill(180,121,239);
+  //ellipse(mouseX,mouseY,50,50);
+  fill(colorX,colorY,colorZ);
+  bezier(0, 0, multiplier1*(mouseX), mouseY, multiplierInt*(mouseX), mouseY, startX, startY);
+  bezier(startX, 0, multiplier1*mouseX, mouseY, multiplierInt*mouseX, mouseY, 0, startY);
+  bezier ((startX/2), 0, (mouseX-orth),startY/4,mouseX+orth,3*startY/4,startX/2,startY);
+  bezier(0, startY/2, startX/4, mouseY+orth, 3*startX/4, mouseY-orth, startX, startY/2);
+  fill(0,30,30,30);
+  rect(0, 0, startX, startY);
+  //bezier(0, 0, 100, 100, -600, -800, 600, 800);
+}
+void colorSwap ()
+{
+ colorX=(int)(Math.random()*255);
+ colorY=(int)(Math.random()*255);
+ colorZ=(int)(Math.random()*255);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,15 +16,49 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-	size(1000,800);
+	size(startX,startY);
 }
+int startX = 1000;
+int startY = 800;
+int multiplierInt = 2;
+int orth = 300;
+int colorX = 25;
+int colorY = 139;
+int colorZ = 2;
+//boolean swap = false;
+//float square = sq(multiplierInt);
+float multiplier1 = multiplierInt/4;
+//float multiplier1 = multiplierInt/4;
 public void draw()
 {
-	background(0,30);
-  ellipse(mouseX,mouseY,50,50);
-  bezier(0, 0, .75f*mouseX, .75f*mouseY, 1.5f*mouseX, 1.5f*mouseY, 1000, 800);
+design();
+}
+public void mousePressed ()
+{
+	colorSwap ();
+	//swap=true;
 }
 
+public void design ()
+{
+	//background(0,30,30);
+	//fill(180,121,239);
+  //ellipse(mouseX,mouseY,50,50);
+  fill(colorX,colorY,colorZ);
+  bezier(0, 0, multiplier1*(mouseX), mouseY, multiplierInt*(mouseX), mouseY, startX, startY);
+  bezier(startX, 0, multiplier1*mouseX, mouseY, multiplierInt*mouseX, mouseY, 0, startY);
+  bezier ((startX/2), 0, (mouseX-orth),startY/4,mouseX+orth,3*startY/4,startX/2,startY);
+  bezier(0, startY/2, startX/4, mouseY+orth, 3*startX/4, mouseY-orth, startX, startY/2);
+  fill(0,30,30,30);
+  rect(0, 0, startX, startY);
+  //bezier(0, 0, 100, 100, -600, -800, 600, 800);
+}
+public void colorSwap ()
+{
+ colorX=(int)(Math.random()*255);
+ colorY=(int)(Math.random()*255);
+ colorZ=(int)(Math.random()*255);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
